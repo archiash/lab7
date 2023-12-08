b3b2cdf77ef47b55acdcbba6cd1b648746324a5c
archiash <archiash.legacy@gmail.com>
2023-12-08 14:37:52 +0700

    Fix Lab7_1

--- lab7_1.cpp
+++ lab7_1.cpp
@@ -2,18 +2,6 @@
 using namespace std;
 
 int main() {
-int i = 0;
-
-while(i < 58){
-
-    char x = 'A'+i;
-
-    cout << x;
-
-    i++;
-
-}
-
     int a  =  20, b = 30;
     int c = 4;
     float output;
